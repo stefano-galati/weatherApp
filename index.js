3b6bd7460a8140c9bdaae567f8547c8b10e06304
galati-stefano <s.galati@jetop.com>
2023-09-09 22:42:17 +0200

    Version 2.0
    Only Turin, but good-looking

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ function changeText(){
 
 function getData(){
 
+    const city = "Turin";
     const target = document.getElementById("target");
     
+    target.innerHTML='<i class="fa-solid fa-spinner fa-spin fa-lg icon white"></i>'
+
     fetch("https://api.open-meteo.com/v1/forecast?latitude=45.0705&longitude=7.6868&hourly=temperature_2m,precipitation&forecast_days=1")
     .then(response => {
         if (response.ok) {
@@ -25,16 +28,18 @@ function getData(){
         target.innerHTML="";
         
         let precipitation = data.hourly.precipitation;
+        let temperature = data.hourly.temperature_2m;
         let totalPrecipitation = precipitation.reduce((sum, elem) => sum+=elem, 0);
+        let averageTemperature = (temperature.reduce((sum, elem) => sum+=elem, 0))/24;
         //target.innerHTML = totalPrecipitation;
 
         if(totalPrecipitation > 0){
             //it rains
-            target.innerHTML='<i class="fa-solid fa-cloud-rain fa-sm icon"></i>';
+            target.innerHTML=`<i class="fa-solid fa-cloud-rain fa-lg icon"></i>`;
         }
         else{
             //no rain
-            target.innerHTML='<i class="fa-solid fa-sun fa-sm icon"></i>';
+            target.innerHTML=`<div class="iconContainer"><i class="fa-solid fa-sun fa-lg icon yellow"></i></div><div class="stylingBox"><div class="iconContainer">Turin</div><div class="iconContainer">${averageTemperature.toFixed(0)}°</div></div>`;
         }
 
 
